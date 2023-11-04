3bf2da0e383a3e920be290d743f74b296495bed5
Pranav Gupta <63167113+prazivi@users.noreply.github.com>
2023-11-04 15:58:46 +0000

    datatypesummary and memory

---
+++ 01_Basics/datatypessummary.js
@@ -0,0 +1,37 @@
+//  Primitive(stack)
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+//heap(non-premitive)
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "pranav",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
