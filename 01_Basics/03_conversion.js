85debe428697c78fd880886d84d307a797e3ef5f
Pranav Gupta <63167113+prazivi@users.noreply.github.com>
2023-11-04 15:27:50 +0000

    conversions

---
+++ 01_Basics/03_conversion.js
@@ -0,0 +1,17 @@
+let score = 33;
+
+console.log(typeof score);
+console.log(typeof (score))
+
+//"33" => 33
+// "33abc" => NaN
+//true => 1 ;false =>0
+
+let isLoggedIn = "pranav";
+let booleanLoggedIn = Boolean(isLoggedIn);
+console.log(booleanLoggedIn);
+
+
+// 1=>trye;  0=>false
+//" =>false
+//"pranav"=>true
