faa12054f8ad33638538ee639c8896639b441c98
Pranav Gupta <63167113+prazivi@users.noreply.github.com>
2023-11-04 15:40:16 +0000

    conversion string

--- 01_Basics/03_conversion.js
+++ 01_Basics/03_conversion.js
@@ -1,17 +1,38 @@
-let score = 33;
+// let score = 33;
 
-console.log(typeof score);
-console.log(typeof (score))
+// console.log(typeof score);
+// console.log(typeof (score))
 
 //"33" => 33
 // "33abc" => NaN
 //true => 1 ;false =>0
 
-let isLoggedIn = "pranav";
-let booleanLoggedIn = Boolean(isLoggedIn);
-console.log(booleanLoggedIn);
+// let isLoggedIn = "pranav";
+// let booleanLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanLoggedIn);
 
 
 // 1=>trye;  0=>false
 //" =>false
 //"pranav"=>true
+
+
+// ***************Operations *******************
+
+let value  =3;
+let negValue = -value;
+// console.log(negValue)
+
+let str1="hello";
+let str2 = "pranav"
+let str3 = str1+str2
+
+console.log(str3) // hello pranav
+
+console.log("1"+2) //12
+console.log(1 + "2") //12
+console.log("1" + 2+2)//122
+console.log(1+2+"2") //32
+console.log(+true) // 1 (conversion)
+
+
