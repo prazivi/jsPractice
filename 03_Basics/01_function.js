4770da52a729ab61f3f7e7543ca36ef0e29b39d6
Pranav Gupta <63167113+prazivi@users.noreply.github.com>
2023-11-08 17:55:40 +0000

    function started

---
+++ 03_Basics/01_function.js
@@ -0,0 +1,29 @@
+function sayMyName(){
+    console.log("Pranav");
+}
+// sayMyName();
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1+number2);
+// }
+
+function addTwoNumbers(number1, number2){
+    let result = number1 + number2;
+    return result;
+}
+
+const result = addTwoNumbers(3,4)//7
+console.log(result); 
+
+
+
+
+
+
+
+
+
+
+
+
+
