6f7515bcb06eb3f598959c0b67e22e4b5f5999e7
Pranav Gupta <63167113+prazivi@users.noreply.github.com>
2023-11-08 17:45:03 +0000

    JSON, Object

--- 02_Basics/04_object.js
+++ 02_Basics/04_object.js
@@ -25,7 +25,65 @@ const obj3 ={...obj1, ...obj2};//{ '1': 'a', '2': 'b', '3': 'A', '4': 'B' }
 
 
 
-console.log(obj3);
+// console.log(obj3);
+
+const users = [
+    {
+        id :1,
+        email: "h@gmail.com"
+    },
+    {
+        id:2,
+        email: "g@gmail.com"
+    },
+    {
+        id:3,
+        email:"i@gmail.com"
+    }
+]
+// console.log(users[1].email); 
+
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));// data type is array
+// console.log(Object.values(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
+//array destructering 
+
+
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseInsturctor: "pranav"
+}
+
+// course.courseInsturctor
+
+const {courseInsturctor: ins} = course
+console.log(ins);
+
+
+
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "pranav")
+
+
+
+//json
+// {
+//     "name" : "prnava",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+
+
+
 
 
 
