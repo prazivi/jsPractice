ec3ba6bbd5814afdf878845dca0eaf9b80aa277e
Pranav Gupta <63167113+prazivi@users.noreply.github.com>
2023-11-09 05:30:53 +0000

    function & object

--- 03_Basics/01_function.js
+++ 03_Basics/01_function.js
@@ -15,6 +15,47 @@ function addTwoNumbers(number1, number2){
 const result = addTwoNumbers(3,4)//7
 console.log(result); 
 
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));//400
+console.log(returnSecondValue([200, 400, 500, 1000]));//400
 
 
 
