139f9753b1136ef1fccae9e39af895e40ef97211
Pranav Gupta <63167113+prazivi@users.noreply.github.com>
2023-11-24 17:58:55 +0000

    function

--- 03_Basics/01_function.js
+++ 03_Basics/01_function.js
@@ -13,7 +13,7 @@ function addTwoNumbers(number1, number2){
 }
 
 const result = addTwoNumbers(3,4)//7
-console.log(result); 
+console.log(result); // undefined as output
 
 function loginUserMessage(username = "sam"){
     if(!username){
