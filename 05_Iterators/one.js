e4b86830610e9aabe142bce479c31706ce0d7c31
Pranav Gupta <63167113+prazivi@users.noreply.github.com>
2023-11-25 08:13:59 +0000

    for loop

---
+++ 05_Iterators/one.js
@@ -0,0 +1,13 @@
+// for loop
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// } 
+//print 1 to n
+let n = 10;
+for(let i=0;i<n;i++){
+    console.log(i);
+}
+
+
