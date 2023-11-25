ce420e6994f7c0cd96fa197b68d09fb5ec8682ab
Pranav Gupta <63167113+prazivi@users.noreply.github.com>
2023-11-25 05:34:47 +0000

    if else

---
+++ 04_controlflow/one.js
@@ -0,0 +1,57 @@
+// if
+/*
+if(condition){
+    statement
+}
+*/
+
+
+// comparsion operator
+
+//  >, <, <=, >=, == , !=, ===
+
+// == (do not check the type of value just compare)
+
+//=== (check the type of value so it is called strictly type equality operator)
+
+// if(2=="2"){
+//     console.log("exectuted")
+// }
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
