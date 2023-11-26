5d5ad4994d961c76d2269940a9ca476e5fa974a0
Pranav Gupta <63167113+prazivi@users.noreply.github.com>
2023-11-26 12:16:07 +0000

    slice,splice,at

---
+++ 06_ArrayMethods/02_Slice.js
@@ -0,0 +1,51 @@
+//slice
+// Array.slice(StartIndex,lastIndex(excluded))
+
+//let num =[3,5,7,9,2];
+
+// let sliced = num.slice(0,3);
+
+// console.log(sliced);
+// console.log(num.slice(-2))
+
+// Splice 
+//used to add new element to the array
+
+//Array.splice(index,deleteValue,valueToBeAdded)
+//Return deleted item in the form of array
+
+// let num =[3,5,7,9,2]
+
+// let updated = num.splice(1,2,1,4)
+// console.log(num)//it is muted the original array
+// console.log(updated);
+
+
+// at method
+// this method can also used in strings
+
+let nums = [23,54,75,85]
+console.log(nums[0]);
+console.log(nums.at(1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
