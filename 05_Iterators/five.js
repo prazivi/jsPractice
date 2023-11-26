109a012673454d61da7fd72c4886921cb66a47c0
Pranav Gupta <63167113+prazivi@users.noreply.github.com>
2023-11-26 05:24:10 +0000

    foreach

---
+++ 05_Iterators/five.js
@@ -0,0 +1,45 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach(function(val){
+// console.log(val);
+// })
+// coding.forEach( (item) =>{
+//     console.log(item)
+// } )
+
+// function printme(item){
+//     console.log(item);
+// }
+// coding.forEach(printme)
+
+// coding.forEach( (item, index, arr)=>{
+//     console.log(item,index,arr)
+// } )
+
+const myCoding = [
+    {
+        languageName : "javaScript",
+        languageName: "js"
+    },
+    {
+        languageName : "Python",
+        languageName: "py"
+    },
+    {
+        languageName : "java",
+        languageName: "j"
+    }
+]
+
+myCoding.forEach( (itme)=>{
+    console.log(itme.languageName)
+} )
+
+
+
+
+
+
+
+
+
