84e8bfb4c8afe371750891b23e713b896548e5fa
Pranav Gupta <63167113+prazivi@users.noreply.github.com>
2023-11-26 04:27:21 +0000

    for in loop

---
+++ 05_Iterators/four.js
@@ -0,0 +1,19 @@
+const myObj = {
+    js: 'javascript',
+    cpp : 'C++',
+    rb: 'ruby',
+    swift: "swift by google"
+}
+for(const key in myObj){
+    //console.log(`${key} shortcut is for ${myObj[key]}`)
+}
+
+const programming = ["js","rb","py","java","cpp"]
+ 
+for(const key in programming){
+    console.log(programming[key])
+}
+
+
+
+
