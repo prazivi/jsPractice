3ce0d5b22b30e6c64e6a4aa34f193c5b5e1796e7
Pranav Gupta <63167113+prazivi@users.noreply.github.com>
2023-11-26 11:58:55 +0000

    reverse, concat,join

---
+++ 06_ArrayMethods/01_Simp.js
@@ -0,0 +1,28 @@
+let letters = ["a","b","c","d","e"]
+
+//reverse (Muted the original array)
+
+// let reverse = letters.reverse();
+// console.log(reverse);
+
+//concat (return non muttating)
+
+let nums= [1,2,3,4,5]
+let concated = nums.concat(letters)
+
+// console.log(concated);
+// console.log(...nums,...letters)// spred operator
+// console.log(typeof(nums))// object
+
+//join (return non mutating array)
+let joined = letters.join('-')
+
+console.log(joined);
+
+
+
+
+
+
+
+
